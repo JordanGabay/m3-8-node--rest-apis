6e72703620cfbbd588e010b54bc43a7e406305e7
Jordan Gabay <gabay.jordan@outlook.com>
2020-07-15 09:59:52 -0400

    Exercise 2 Complete

--- handlers/clientHandlers.js
+++ handlers/clientHandlers.js
@@ -1,4 +1,43 @@
 const { v4: uuidv4 } = require('uuid');
 const { clients } = require('../data/clients');
 
+
 // write your handlers here...
+
+const handleRoot = (req, res) => {
+    res.status(200).send('Hello! Welcome to my first Insomnia example')
+}
+
+const getHandleClient = (req, res) => {
+    res.status(200).json(clients)
+}
+
+const getHandleClients = (req, res) => {
+    const {id} = req.params
+    client = clients.filter(client => id === client.id)
+    res.status(200).json(client)
+}
+
+const postHandleClient = (req, res) => {
+    const {email} = req.body
+    let emailExists = clients.some(client => email === client.email)
+    if(!emailExists) {
+        const id = uuidv4()
+        res.status(201).json({id, ...req.body})
+    } else {
+        res.status(304).send('An account with this email address already exists!')
+    }
+}
+
+const deleteHandleClient = (req, res) => {
+    const {id} = req.params
+    const index = clients.indexOf(client => id === client.id)
+    clients.splice(index, 1)
+    res.status(200).json(clients)
+}
+
+const errorPage = (req, res) => {
+    res.status(404).send("Hey... I don't think you're on the right page? Try again!")
+}
+
+module.exports = { handleRoot, getHandleClient, getHandleClients, postHandleClient, deleteHandleClient, errorPage };
--- server.js
+++ server.js
@@ -4,6 +4,10 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 
+const {
+  handleRoot, getHandleClient, getHandleClients, postHandleClient, deleteHandleClient, errorPage
+} = require('./handlers/clientHandlers')
+
 express()
   .use(function (req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
@@ -18,6 +22,17 @@ express()
   .use(bodyParser.json())
   .use(express.urlencoded({ extended: false }))
 
+
   // endpoints
+
+  // Client Handlers
+  .get('/', handleRoot) // Homepage 
+  .get('/clients', getHandleClient) // Shows id of all clients
+  .get('/clients/:id', getHandleClients) // allows you to put id into the url to search for specific client
+  .post('/clients/:email', postHandleClient) // shows an id when you put the email
+  .delete('/clients/:id', deleteHandleClient) // deletes last client from the list
+  .get('*', errorPage) // Created an error page if user goes off track 
+
+  // Hangman Handlers
 
   .listen(8000, () => console.log(`Listening on port 8000`));
